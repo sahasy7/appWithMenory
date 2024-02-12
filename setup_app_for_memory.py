ad1f5e11658c4b0cc56629720ff4982782a763de
sahasy7 <74914769+sahasy7@users.noreply.github.com>
2024-02-12 11:21:44 +0530

    Update setup_app_for_memory.py

--- setup_app_for_memory.py
+++ setup_app_for_memory.py
@@ -111,7 +111,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
   with st.chat_message("assistant"):
     with st.spinner("Thinking..."):
       response = st.session_state.chat_engine.run(prompt)
-      st.write(response.response)
-      message = {"role": "assistant", "content": response.response}
+      st.write(response)
+      message = {"role": "assistant", "content": response}
       st.session_state.messages.append(
           message)  # Add response to message history
